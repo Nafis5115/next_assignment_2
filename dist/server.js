607427446ae46d0b30e26dca2a3a35f48c6b5131
Nafis5115 <72686804+Nafis5115@users.noreply.github.com>
2026-08-06 16:01:12 +0600

    fixing files

--- dist/server.js
+++
@@ -1,7 +0,0 @@
-import express, {} from "express";
-const app = express();
-const port = process.env.PORT || 3000;
-app.get("/", (req, res) => { });
-app.listen(port, () => {
-    console.log(`Server Running on port ${port}`);
-});
